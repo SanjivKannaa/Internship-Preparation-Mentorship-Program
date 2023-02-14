946a296da13a8622ae911f745aa1162facae2707
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2023-02-14 23:48:02 +0530

    commit

--- week2/1_linkedlist/linkedlist_4.cpp
+++ week2/1_linkedlist/linkedlist_4.cpp
@@ -1,7 +1,5 @@
 // Check if a singly linked list is a Palindrome
-
 #include <bits/stdc++.h>
-
 using namespace std;
 
 struct node {
@@ -9,48 +7,47 @@ struct node {
     struct node* next;
 };
 
-struct node *head1 = NULL;
-struct node *head2 = NULL;
+struct node *head = NULL;
 
-void insert_first(struct node* headi, int data){
+void insert_first(int data){
     struct node* newnode = new node();
     newnode->data = data;
-    newnode->next = headi;
-    headi = newnode;
+    newnode->next = head;
+    head = newnode;
 }
 
-void insert_last(struct node* headi, int data){
+void insert_last(int data){
     struct node* newnode = new node();
     newnode->data = data;
     newnode->next = NULL;
-    struct node* current = headi;
+    struct node* current = head;
     while (current->next!=NULL){
         current = current->next;
     }
     current->next = newnode;
 }
 
-int delete_first(struct node* headi){
-    struct node* temp = headi;
-    headi = headi->next;
+int delete_first(){
+    struct node* temp = head;
+    head = head->next;
     int data = temp->data;
     free(temp);
     return data;
 }
 
-int delete_last(struct node* headi){
-    if (headi==NULL){
+int delete_last(){
+    if (head==NULL){
         cout << "linkedlist is empty";
 	    return -1;
     }
-    if (headi->next==NULL){
-	    struct node* temp = headi;
-	    headi = NULL;
+    if (head->next==NULL){
+	    struct node* temp = head;
+	    head = NULL;
 	    int data = temp->data;
 	    free(temp);
 	    return data;
     }
-    struct node* current = headi;
+    struct node* current = head;
     while(current->next->next!=NULL){
         current = current->next;
     }
@@ -59,4 +56,40 @@ int delete_last(struct node* headi){
     current->next = NULL;
     free(temp);
     return data;
+}
+
+bool is_palindrome(node* head){
+    node* slow = head;
+    stack<int> s;
+    while (slow != NULL) {
+        s.push(slow->data);
+        slow = slow->next;
+    }
+    while (head != NULL) {
+        int i = s.top();
+        s.pop();
+        if (head->data != i) {
+            return false;
+        }
+        head = head->next;
+    } 
+    return true;
+}
+
+int main(){
+    insert_first(1);
+    insert_last(2);
+    insert_last(3);
+    insert_last(4);
+    insert_last(5);
+    insert_last(4);
+    insert_last(3);
+    insert_last(2);
+    insert_last(1);
+    if (is_palindrome(head)){
+        cout << "is a palindome";
+    }else{
+        cout << "is not a palindome";
+    }
+    return 0;
 }
--- week2/1_linkedlist/linkedlist_9.cpp
+++ week2/1_linkedlist/linkedlist_9.cpp
@@ -1,7 +1,5 @@
 // Find the intersection point of two Linked Lists
-
 #include <bits/stdc++.h>
-
 using namespace std;
 
 struct node {
@@ -9,48 +7,47 @@ struct node {
     struct node* next;
 };
 
-struct node *head1 = NULL;
-struct node *head2 = NULL;
+struct node *head = NULL;
 
-void insert_first(struct node* headi, int data){
+void insert_first(int data){
     struct node* newnode = new node();
     newnode->data = data;
-    newnode->next = headi;
-    headi = newnode;
+    newnode->next = head;
+    head = newnode;
 }
 
-void insert_last(struct node* headi, int data){
+void insert_last(int data){
     struct node* newnode = new node();
     newnode->data = data;
     newnode->next = NULL;
-    struct node* current = headi;
+    struct node* current = head;
     while (current->next!=NULL){
         current = current->next;
     }
     current->next = newnode;
 }
 
-int delete_first(struct node* headi){
-    struct node* temp = headi;
-    headi = headi->next;
+int delete_first(){
+    struct node* temp = head;
+    head = head->next;
     int data = temp->data;
     free(temp);
     return data;
 }
 
-int delete_last(struct node* headi){
-    if (headi==NULL){
+int delete_last(){
+    if (head==NULL){
         cout << "linkedlist is empty";
 	    return -1;
     }
-    if (headi->next==NULL){
-	    struct node* temp = headi;
-	    headi = NULL;
+    if (head->next==NULL){
+	    struct node* temp = head;
+	    head = NULL;
 	    int data = temp->data;
 	    free(temp);
 	    return data;
     }
-    struct node* current = headi;
+    struct node* current = head;
     while(current->next->next!=NULL){
         current = current->next;
     }
@@ -59,4 +56,17 @@ int delete_last(struct node* headi){
     current->next = NULL;
     free(temp);
     return data;
+}
+
+node* getIntesectionNode(node* head1, node* head2){
+    while (head2) {
+        node* temp = head1;
+        while (temp) {
+            if (temp == head2)
+                return head2;
+            temp = temp->next;
+        }
+        head2 = head2->next;
+    }
+    return NULL;
 }
