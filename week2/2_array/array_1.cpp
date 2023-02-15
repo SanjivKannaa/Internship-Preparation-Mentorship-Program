70f16efc4d0380b5ad15aefe47f980ba1d05234f
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2023-02-16 00:48:47 +0530

    commit

--- week2/2_array/array_1.cpp
+++ week2/2_array/array_1.cpp
@@ -3,18 +3,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void array_intersection(int arr1[], int arr2[], int n1, int n2){
-    cout << "duh";
-    int i=0;
-    int j=0;
-    while(i<n1 && j<n2){
+void array_intersection(int arr1[], int arr2[], int m, int n){
+    int i = 0, j = 0;
+    while (i < m && j < n) {
         if (arr1[i] < arr2[j]){
             i++;
-        }
-        if (arr1[i] > arr2[j]){
+        }else if (arr2[j] < arr1[i]){
             j++;
-        }
-        if (arr1[i] == arr2[i]){
+        }else{
             cout << arr2[j] << " ";
             i++;
             j++;
@@ -22,27 +18,24 @@ void array_intersection(int arr1[], int arr2[], int n1, int n2){
     }
 }
 
-// void array_union(int arr1[], int arr2[], int n1, int n2){
-//     int i=0, j=0;
-//     while(i<n1 && j<n2){
-//         if (arr1[i] < arr2[j]){
-//             cout << arr1[i++] << " ";
-//         }
-//         if (arr1[j] > arr2[i]){
-//             cout << arr2[j++] << " ";
-//         }
-//         if (arr1[i] == arr2[i]){
-//             cout << arr2[j++] << " ";
-//             i++;
-//         }
-//     }
-//     while (i<n1){
-//         cout << arr1[i++] << " ";
-//     }
-//     while (j<n2){
-//         cout << arr2[j++] << " ";
-//     }
-// }
+void array_union(int arr1[], int arr2[], int m, int n){
+    int i = 0, j = 0;
+    while (i < m && j < n) {
+        if (arr1[i] < arr2[j]){
+            cout << arr1[i++] << " ";
+        }else if (arr2[j] < arr1[i]){
+            cout << arr2[j++] << " ";
+        }else{
+            cout << arr2[j++] << " ";
+            i++;
+        }
+    }
+    while (i < m)
+        cout << arr1[i++] << " ";
+ 
+    while (j < n)
+        cout << arr2[j++] << " ";
+}
 
 
 
@@ -51,10 +44,10 @@ int main(){
     int arr2[] = {4, 5, 6, 7, 8};
     int n1 = sizeof(arr1)/sizeof(arr1[0]);
     int n2 = sizeof(arr2)/sizeof(arr2[0]);
-    cout << "hi";
     cout << "intersection: ";
     array_intersection(arr1, arr2, n1, n2);
-    cout << "union: ";
-    // array_union(arr1, arr2, n1, n2);
+    cout << "\nunion: ";
+    array_union(arr1, arr2, n1, n2);
+    cout << endl;
     return 0;
 }
