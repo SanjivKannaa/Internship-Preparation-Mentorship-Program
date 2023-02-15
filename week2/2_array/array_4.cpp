e3d6ad6e819d3877b9bbfc34c851f1fe7e5c2838
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2023-02-15 15:28:22 +0530

    commit

--- week2/2_array/array_4.cpp
+++ week2/2_array/array_4.cpp
@@ -2,23 +2,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> get2NonRepeatingNos(int nums[], int n){
-	sort(nums, nums + n);
-	vector<int> ans;
-	for (int i = 0; i < n - 1; i = i + 2) {
-		if (nums[i] != nums[i + 1]) {
-			ans.push_back(nums[i]);
-			i = i - 1;
-		}
-	}
-	if (ans.size() == 1)
-		ans.push_back(nums[n - 1]);
-	return ans;
-}
-
 int main(){
-	int arr[] = { 2, 3, 7, 9, 11, 2, 3, 11 };
+	int arr[] = { 2, 3, 7, 9, 11, 2, 3, 11 }; // 2, 2, 3, 3, 7, 9, 11, 11
 	int n = sizeof(arr) / sizeof(*arr);
-	vector<int> ans = get2NonRepeatingNos(arr, n);
-	cout << "The non-repeating elements are " << ans[0] << " and " << ans[1] << endl;
+    sort(arr, arr + n);
+    int i=0;
+    int j=1;
+    while (j<n){
+        if (arr[i] == arr[j]){
+            j++;
+        }else{
+            if (i+1==j){
+                cout << arr[i] << endl;
+                // return 0;
+            }
+            i = j;
+        }
+    }
 }
