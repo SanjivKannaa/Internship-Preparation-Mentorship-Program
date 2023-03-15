a0bd889bc093e1fad228e4675d31f49c34b44302
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2023-03-15 15:17:02 +0530

    update

--- week1/day2/BinarySearch6.cpp
+++ week1/day2/BinarySearch6.cpp
@@ -6,5 +6,17 @@
 using namespace std;
 
 int main(){
-    
+    int count=0;
+    int arr[] = {1, 2, 2, 3, 3, 3, 4, 4, 5};
+    int i = 0;
+    int j = 0;
+    int c=0;
+    while(j<sizeof(arr)/sizeof(arr[0])){
+        if (arr[i] == arr[j]){
+            c++;
+            j++;
+        }else{
+            
+        }
+    }
 }
