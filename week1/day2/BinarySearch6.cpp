64d9af5da73b52b1ed2e6d4046b5d751a3fb8d1f
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2023-03-18 23:03:23 +0530

    update

--- week1/day2/BinarySearch6.cpp
+++ week1/day2/BinarySearch6.cpp
@@ -9,11 +9,11 @@ int main(){
     int count=0;
     int arr[] = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5};
     int i = 0;
-    int j = 0;
+    int j = 1;
     int c=0;
     int number = arr[0];
     while(j<sizeof(arr)/sizeof(arr[0])){
-        if (arr[i] == arr[j]){
+        if (arr[i] == arr[j-1]){
             c++;
             j++;
         }else{
--- week1/day2/BinarySearch9.cpp
+++ week1/day2/BinarySearch9.cpp
@@ -1,1 +1,27 @@
 // Find First and Last Position of Element in Sorted Array
+#include<iostream>
+#include<algorithm>
+
+
+using namespace std;
+
+int main(){
+    int arr[] = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 8};
+    int n;
+    int len = sizeof(arr)/sizeof(arr[0]);
+    cin >> n;
+    int start=0, end=0;
+    for (int i=1;i<len;i++){
+        if (arr[i]==n && arr[i-1]!=n){
+            start=i;
+        }
+        if (arr[i]==n && (arr[i+1]!=n || n==len-1)){
+            end=i;
+        }
+    }
+    if (arr[len-1]==n && arr[len-2]!=n){
+        start = len-1;
+        end = len-1;
+    }
+    cout << n << " starts at " << start << " and ends at " << end;
+}
