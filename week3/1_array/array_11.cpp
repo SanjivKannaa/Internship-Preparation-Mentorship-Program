ae075ca3851573fdef2e80010c7aa2143d897f44
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2023-03-19 08:40:20 +0530

    update

--- week3/1_array/array_11.cpp
+++ week3/1_array/array_11.cpp
@@ -5,11 +5,17 @@ using namespace std;
 
 int main(){
     int arr[] = {1, 2, 3, 4, 5, 6, 4, 7, 8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int sum;
+    cin >> sum;
     sort(arr, arr + (sizeof(arr)/sizeof(arr[0])));
-    for(int i=0;i<sizeof(arr)/sizeof(arr[0]);i++){
-        if (arr[i]==arr[i+1]){
-            cout << arr[i] << endl;
-            return 0;
+    for (int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            int x = sum - (arr[i]+arr[j]);
+            int search_result = binary_search(&arr[0], &arr[n-1], x);
+            if (binary_search(&arr[0], &arr[n-1], x) && ((sum-arr[i]-arr[j])!=arr[i] && (sum-arr[i]-arr[j])!=arr[j])){
+                cout << arr[i] << " + " << arr[j] << " + " << sum-arr[i]-arr[j] << " = " << sum << endl;
+            }
         }
     }
 }
