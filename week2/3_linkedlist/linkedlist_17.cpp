0d7da8cabdce516221b845cec734ab8c3ab38c91
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2023-03-24 13:08:55 +0530

    update

--- week2/3_linkedlist/linkedlist_17.cpp
+++ week2/3_linkedlist/linkedlist_17.cpp
@@ -4,19 +4,74 @@
 using namespace std;
 
 struct node {
-	int key;
-	struct node* next;
+    int data;
+    struct node* next;
 };
 
-struct node* head = NULL;
-struct node* result = NULL;
-struct node* newNode(int key){
-	struct node* temp = new struct node;
-	temp->key = key;
-	temp->next = NULL;
-	return temp;
+struct node *head = NULL;
+
+void insert_first(int data){
+    struct node* newnode = new node();
+    newnode->data = data;
+    newnode->next = head;
+    head = newnode;
 }
 
-int main(){
-    
+void insert_last(int data){
+    struct node* newnode = new node();
+    newnode->data = data;
+    newnode->next = NULL;
+    struct node* current = head;
+    if (current==NULL){
+        head=newnode;
+        return ;
+    }
+    while (current->next!=NULL){
+        current = current->next;
+    }
+    current->next = newnode;
+}
+
+int delete_first(){
+    struct node* temp = head;
+    head = head->next;
+    int data = temp->data;
+    free(temp);
+    return data;
+}
+
+int delete_last(){
+    if (head==NULL){
+        cout << "linkedlist is empty";
+	    return -1;
+    }
+    if (head->next==NULL){
+	    struct node* temp = head;
+	    head = NULL;
+	    int data = temp->data;
+	    free(temp);
+	    return data;
+    }
+    struct node* current = head;
+    while(current->next->next!=NULL){
+        current = current->next;
+    }
+    struct node* temp = current->next;
+    int data = temp->data;
+    current->next = NULL;
+    free(temp);
+    return data;
+}
+
+void printll(){
+    struct node *current = head;
+    if (current==NULL){
+        cout << "NULL" << endl;
+        return ;
+    }
+    while(current!=NULL){
+        cout << "->" << current->data;
+        current=current->next;
+    }
+    cout << endl;
 }
